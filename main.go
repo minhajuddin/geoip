2acedc50c863b34be8131f66a51024aa82062e18
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2012-11-26 21:35:13 +0530

    Wired up search

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -12,16 +13,26 @@ import (
 const DB_SIZE = 110000
 
 //NOTE:Can this be stored in a map somehow?
-var db []Location
+var db Locations
 
 func main() {
+	loadDb()
+	sortDb()
+	for _, x := range db {
+		log.Println(x)
+	}
+
+	log.Println(SearchLocation(16779265))
+}
+
+func loadDb() {
 	file, err := os.Open("countries.txt")
 	defer file.Close()
 	r := bufio.NewReader(file)
 	if err != nil {
 		log.Fatalln("unable to open countries.txt", err)
 	}
-	db = make([]Location, DB_SIZE)
+	db = make(Locations, 0, DB_SIZE)
 	for {
 		line, err := r.ReadString('\n')
 		line = strings.Trim(line, "\n")
@@ -50,19 +61,46 @@ func main() {
 			continue
 		}
 
-		l := &Location{
-			Start: int32(start),
-			End:   int32(end),
-			Code:  tokens[2],
+		l := Location{
+			IpStart:     int32(start),
+			IpEnd:       int32(end),
+			CountryCode: tokens[2],
 		}
 
-		log.Println(l)
-
+		db = append(db, l)
 	}
 }
 
+func sortDb() {
+	sort.Sort(db)
+}
+
 type Location struct {
-	Code  string
-	Start int32
-	End   int32
+	CountryCode string
+	IpStart     int32
+	IpEnd       int32
+}
+
+//sort interface
+type Locations []Location
+
+func (self Locations) Len() int           { return len(self) }
+func (self Locations) Swap(i, j int)      { self[i], self[j] = self[j], self[i] }
+func (self Locations) Less(i, j int) bool { return self[i].IpStart < self[j].IpStart }
+
+//search
+//copied straight from sort/search.go
+func SearchLocation(ip int32) string {
+	i, j := 0, len(db)
+	for i < j {
+		h := i + (j-i)/2 // avoid overflow when computing h
+		// i ≤ h < j
+		if db[h].IpStart < ip {
+			i = h + 1 // preserves f(i-1) == false
+		} else {
+			j = h // preserves f(j) == true
+		}
+	}
+	// i == j, f(i-1) == false, and f(j) (= f(i)) == true  =>  answer is i.
+	return db[i].CountryCode
 }
